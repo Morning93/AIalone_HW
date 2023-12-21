fe5cb5a5924d1cd60b0087bec7a514742f0844d3
inst-Morning93 <inst.mynameet93@gmail.com>
2023-12-21 11:24:35 +0900

    M: argument 추가(data, model)

--- utils/HWparser.py
+++ utils/HWparser.py
@@ -1,29 +1,26 @@
 import argparse
 
+# hyper-parameter 선언 
 def parse_train_args():
-    parser = argparse.ArgumentParser() # parser 객체 만들기
-    # parser에 argument들 추가
-    parser.add_argument('--image_size', type = int, default = 28,
-                        help = 'input imags size')
-    parser.add_argument('--batch_size', type = int, default = 100)
-    parser.add_argument('--hidden_size', type = int, default = 500,
-                        help = 'hidden layer의 output_features')
-    parser.add_argument('--num_class', type = int, default = 10)
-    parser.add_argument('--lr', type = float, default = 0.001,
-                        help = 'learning rate')
-    parser.add_argument('--epoch', type = int, default = 3)
-    parser.add_argument('--results_folder_path', type = str, default = 'results',
-                        help = '학습 결과를 저장 할 최상위 폴더 경로')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_size', type=int, default=28, help='image data size for training and inferencing')
+    parser.add_argument('--batch_size', type=int, default=100)
+    parser.add_argument('--hidden_size', type=int, default=500)
+    parser.add_argument('--num_class', type=int, default=10)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--epoch', type=int, default=3)
+    parser.add_argument('--results_folder_path', type=str, default='results')
 
-    #args 변수에 파싱된 arguments들 연결!
-    args = parser.parse_args()
+    # 사용하는 데이터셋에 관한 argument 추가
+    parser.add_argument('--model_type', type = str, default = 'mlp', choices = ['mlp', 'lenet'])
+    parser.add_argument('--data', type = str, default = 'MNIST', choices = ['MNIST', 'CIFAR'])
+    
+    args = parser.parse_args() 
     return args
-
 
 def parse_infer_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--target_image_path', type= str, default = 'mnist_test.png', help = 'infer용 데이터 경로')
-    parser.add_argument('--load_folder', nargs = '+', type= str, help = '학습 결과를 담고있는 폴더 경로')
-
-    args = parser.parse_args()
+    parser.add_argument('--target_image_path', type=str, help='추론을 원하는 데이터의 경로')
+    parser.add_argument('--load_folder', nargs='+',  help='학습된 모델을 담고 있는 폴더의 경로')
+    args = parser.parse_args() 
     return args

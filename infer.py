5d5a96c66d906d38c9d33d79c30c648dee2cbfbe
inst-Morning93 <inst.mynameet93@gmail.com>
2023-12-21 11:23:14 +0900

    M: get_model input parameter 수정

--- infer.py
+++ infer.py
@@ -1,91 +1,44 @@
 # 필요한 패키지를 import 
-import json
-import argparse
-import torch
-import torch.nn as nn 
-from utils.HWparser import parse_infer_args, parse_train_args
-from utils.utils import get_ckpt_path, get_hparam_path
-from utils.get_modules import prepare_image, get_model, get_transform
-'''
 import os 
+import json
+import torch
+import argparse
+import torch.nn as nn 
 from PIL import Image
 from torchvision.transforms import Resize
 from torchvision.transforms import Compose
 from torchvision.transforms import ToTensor
-'''
-def main():
-    args = parse_infer_args()
+
+from utils.HWparser import parse_train_args
+from utils.utils import get_loadfolder_path, load_image
+from utils.get_modules import get_transform, get_model
+
+def main(): 
+    args = parse_train_args()
     args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    train_args = parse_train_args()
-    # 전처리 과정 등에서 학습 당시의 하이퍼 파라미터들이 필요하므로 소환!!
-    ckpt_path = get_ckpt_path(args)
-    hparam_path = get_hparam_path(args)
-    # target_image_path = 'mnist_test.png'
-    # 저장한 hparam을 load 
-    with open(hparam_path, 'r') as f:
-        train_args = argparse.Namespace(**json.load(f))
-    # load_folder = os.path.join('results', '00')
-    '''
+
+    get_loadfolder_path(args)
     ckpt_path = os.path.join(args.load_folder, 'best_model.ckpt')
-    hparam_path = os.path.join(args.load_folder, 'hparam.txt')
-    '''
-    '''
+    hparam_path = os.path.join(args.load_folder, 'hparam.json')
+
+    # 저장한 hparam을 load 
     with open(hparam_path, 'r') as f : 
-        data = f.read().split('|')
-    '''
-    # image_size = int(data[0])
-    # hidden_size = int(data[2])
-    # num_class = int(data[3])
-    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        train_args = argparse.Namespace(**json.load(f))
 
     # 1. Test data를 위한 경우 > 구현하지는 X
         # 기존에 만들었던 Dataloader를 그대로 가져오면 됨 
         # 그렇게 때문에 test.py와 같은 파일로 따로 보관 
     # 2. 실제 데이터를 받는 경우 
     # 학습했던 data 전처리 과정을 그대로 가져와야함 
-    image = prepare_image(args)
+    image = load_image(args)
+
     transform = get_transform(train_args)
-    image = transform(image).to(args.device)
-    '''
-    image = Image.open(args.target_image_path)
-    image = image.convert('L')
-    
-    transform = get_transform(args)
-    
-    transform = Compose([Resize((train_args.image_size, 
-                                train_args.image_size)), 
-                        ToTensor()])
     # dataloader의 역할에 상응하는 다른 모듈을 만들어야 함 
     image = transform(image).to(args.device)
-    '''    
 
-    # 이미 만들어진 AI 모델 설계도를 가져오고 
-    '''     
-    class myMLP(nn.Module):
-        def __init__(self, image_size, hidden_size, num_class): 
-            super().__init__()
-            self.image_size = image_size 
-            self.mlp1 = nn.Linear(in_features=image_size*image_size, out_features=hidden_size)
-            self.mlp2 = nn.Linear(in_features=hidden_size, out_features=hidden_size)
-            self.mlp3 = nn.Linear(in_features=hidden_size, out_features=hidden_size)
-            self.mlp4 = nn.Linear(in_features=hidden_size, out_features=num_class)
 
-        def forward(self, x): # x : [batch_size, height, width]
-            batch_size = x.shape[0]
-            x = torch.reshape(x, (batch_size, self.image_size * self.image_size)) # x : [batch_size, 784]
-            x = self.mlp1(x)
-            x = self.mlp2(x)
-            x = self.mlp3(x)
-            x = self.mlp4(x) # x : [batch_size, 10]
-            return x 
-    ''' 
     # AI 모델 객체 생성 (과정에서 hyper-parameter가 사용)
-    model = get_model(train_args)
-    '''
-    model = myMLP(image_size=train_args.image_size, 
-                hidden_size=train_args.hidden_size, 
-                num_class=train_args.num_class).to(args.device) 
-    '''
+    model = get_model(args)
 
     # 학습된 weight를 생성한 AI 모델 객체에 넣어주기 
     ckpt = torch.load(ckpt_path)
@@ -99,6 +52,6 @@ def main():
     prob = torch.max(nn.functional.softmax(output, dim=1))*100
 
     print(f'모델은 이 이미지를 {prob}%의 확률로 {output_index+1} 라고 합니다.')
- 
-if __name__ == '__main__':
-    main()
+    
+if __name__ == '__main__': 
+    main() 
